430ceea0409266c7c0618798e4153e22e4ff6870
Adam Pala <adam.w.pala@gmail.com>
2020-07-17 18:24:12 -0500

    Completed use distinct

---
+++ 05-use-distinct.sql
@@ -0,0 +1,15 @@
+/*
+|--- Use DISTINCT ---|
+*/
+ 
+-- Step 1
+SELECT DISTINCT composer
+FROM track;
+
+-- Step 2
+SELECT DISTINCT billing_postal_code
+FROM invoice;
+
+-- Step 3
+SELECT DISTINCT company
+FROM customer;
